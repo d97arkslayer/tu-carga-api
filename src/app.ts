c58cde8a234ce938ac99131f3ae462433da01495
Esteban Herrera Orozco <d97ark.dev@gmail.com>
2025-03-06 04:25:41 -0500

    feat: create the first delivery for march 8

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import routes from './routes';
 import dotenv from 'dotenv';
 import { errorHandler } from '@middlewares/error.middleware';
 import helmet from 'helmet';
+import cors from 'cors';
 
 // Cargar variables de entorno
 dotenv.config();
@@ -15,6 +16,15 @@ app.use(helmet());
 
 // Middlewares
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
 
 // Rutas
 app.use('/api', routes);
